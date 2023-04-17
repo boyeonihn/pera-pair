fb3421b7366105504fa9af98cef25c61268ba4ad
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-18 07:10:01 +0900

    build: set pug as view engine

--- src/server.js
+++ src/server.js
@@ -9,8 +9,10 @@ import { userRouter } from './routers/userRouter';
 const PORT = 4000;
 const app = express();
 const logger = morgan('dev');
-app.use(logger);
 
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
+app.use(logger);
 app.use('/', global);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
