e5a92c4b259af97ca5ea35ef63a835c7977705a7
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-22 07:12:03 +0900

    refactor: refactor routers code

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import { see, edit, upload, remove } from '../controllers/videoController';
+import {
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+} from '../controllers/videoController';
 
 export const videoRouter = express.Router();
 
-videoRouter.get('/upload', upload); // need to put this on the top because express might consider "upload" as a valid :id parameter
-videoRouter.get('/:id', see);
-videoRouter.get('/:id/edit', edit);
-videoRouter.get('/:id/remove', remove);
+videoRouter.route('/upload').get(getUpload).post(postUpload); // need to put this on the top because express might consider "upload" as a valid :id parameter
+videoRouter.get('/:id', watch);
+// videoRouter.get('/:id/edit', getEdit);
+// videoRouter.post('/:id/edit', postEdit);
+// You can do both post/get request in one line of code
+
+videoRouter.route('/:id/edit').get(getEdit).post(postEdit);
+// videoRouter.get('/:id/remove', remove);
