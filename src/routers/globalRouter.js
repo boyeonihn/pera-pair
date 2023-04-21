663f7298f66c54eb81f5e9ef01b985959420bd54
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-22 07:12:03 +0900

    refactor: refactor routers code

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { join, login } from '../controllers/userController';
-import { trendingVideos as home, search } from '../controllers/videoController';
+import { home as home } from '../controllers/videoController';
 
 export const globalRouter = express.Router();
 
 globalRouter.get('/', home);
 globalRouter.get('/join', join);
 globalRouter.get('/login', login);
-globalRouter.get('/search', search);
+// globalRouter.get('/search', search);
