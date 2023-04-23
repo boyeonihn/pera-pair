c3777db49f5864ccb1096c845d50fb545b46dac1
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-23 15:05:04 +0900

    build: fix mixin video template

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,15 +5,16 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from '../controllers/videoController';
 
 export const videoRouter = express.Router();
 
 videoRouter.route('/upload').get(getUpload).post(postUpload); // need to put this on the top because express might consider "upload" as a valid :id parameter
-videoRouter.get('/:id', watch);
+videoRouter.get('/:id([0-9a-f]{24})', watch);
 // videoRouter.get('/:id/edit', getEdit);
 // videoRouter.post('/:id/edit', postEdit);
 // You can do both post/get request in one line of code
 
 videoRouter.route('/:id([0-9a-f]{24})/edit').get(getEdit).post(postEdit);
-// videoRouter.get('/:id/remove', remove);
+videoRouter.get('/:id/delete', deleteVideo);
