852d848b9c21ab605fffa6d3c2a8f0e934c08194
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-23 15:41:09 +0900

    feat: create and apply hashtag static function to Video schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -12,5 +12,18 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+//middleware must go before the model is made
+
+// videoSchema.pre('save', async function () {
+//   console.log('we are about to save:', this);
+//   this.hashtags = this.hashtag[0]
+//     .split(',')
 //     .map((word) => (word.startsWith('#') ? word : `#${word.trim()}`));
+// });
+
+videoSchema.static('formatHashtags', function (hashtags) {
+  return hashtags
+    .split(',')
+    .map((word) => (word.startsWith('#') ? word : `#${word.trim()}`));
+});
 export const Video = mongoose.model('Video', videoSchema);
