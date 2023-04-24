bca9213b7cbc967cf3ce8b86b254bef52b455999
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-24 11:27:01 +0900

    chore: rename globalRouter to rootRouter

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 
 import express from 'express';
 import morgan from 'morgan';
-import { globalRouter as global } from './routers/globalRouter';
+import { rootRouter } from './routers/rootRouter';
 import { videoRouter } from './routers/videoRouter';
 import { userRouter } from './routers/userRouter';
 
@@ -14,7 +14,7 @@ app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use('/', global);
+app.use('/', rootRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
 
