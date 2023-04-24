1132066ca53febd5560ac7486a624cfe2b65c801
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-24 13:47:40 +0900

    refactor: add status code to videoController responses

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -27,7 +27,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render('404', { pageTitle: 'Video Not Found' });
+    return res.status(404).render('404', { pageTitle: 'Video Not Found' });
   }
 
   await Video.findByIdAndUpdate(id, {
@@ -44,7 +44,7 @@ export const deleteVideo = async (req, res) => {
   const video = await Video.exists({ _id: id });
 
   if (!video) {
-    return res.render('404', { pageTitle: 'Video Not Found' });
+    return res.status(404).render('404', { pageTitle: 'Video Not Found' });
   }
   await Video.findByIdAndDelete(id);
   return res.redirect(`/`);
@@ -74,7 +74,7 @@ export const postUpload = async (req, res) => {
     return res.redirect(`/`);
   } catch (error) {
     const errorMsg = error._message;
-    return res.render('upload', {
+    return res.status(400).render('upload', {
       pageTitle: `Uploading Video`,
       error: errorMsg,
     });
