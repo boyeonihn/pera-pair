afbaa0d348557e09656c61bf2e99c7600a9e30bd
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-24 13:48:54 +0900

    build: build postJoin functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,16 +2,35 @@ import { User } from '../models/User';
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
 
 export const postJoin = async (req, res) => {
-  const { name, email, username, password, location } = req.body;
-
-  await User.create({
-    email,
-    username,
-    password,
-    location,
-    name,
-  });
-  return res.redirect('/login');
+  const { email, name, password, location, passwordConfirm } = req.body;
+  const emailExists = await User.exists({ email });
+  const pageTitle = 'Join';
+  if (emailExists) {
+    return res.status(400).render('join', {
+      pageTitle,
+      errorMessage: 'This email is already taken.',
+    });
+  } else if (password !== passwordConfirm) {
+    return res.status(400).render('join', {
+      pageTitle,
+      errorMessage: 'Passwords do not match. Please retype your password.',
+    });
+  }
+  try {
+    await User.create({
+      email,
+      password,
+      location,
+      name,
+    });
+    return res.redirect('/login');
+  } catch (error) {
+    const errorMsg = error._message;
+    return res.status(400).render('join', {
+      pageTitle,
+      errorMessage: errorMsg,
+    });
+  }
 };
 
 export const edit = (req, res) => res.send('Editing Profile');
