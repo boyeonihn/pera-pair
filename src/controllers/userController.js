36c61a2bd414ce586b0f4995296e8e7922f9f72e
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-24 13:49:21 +0900

    build: build post/get methods for login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import { User } from '../models/User';
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
+import bcrypt from 'bcrypt';
 
 export const postJoin = async (req, res) => {
   const { email, name, password, location, passwordConfirm } = req.body;
@@ -35,6 +36,28 @@ export const postJoin = async (req, res) => {
 
 export const edit = (req, res) => res.send('Editing Profile');
 export const remove = (req, res) => res.send('Delete User');
-export const login = (req, res) => res.render('login', { pageTitle: 'Login' });
+export const getLogin = (req, res) =>
+  res.render('login', { pageTitle: 'Login' });
+export const postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const pageTitle = 'Login';
+
+  if (!user) {
+    return res.status(400).render('login', {
+      pageTitle,
+      errorMessage: 'An account with this username does not exist',
+    });
+  }
+
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render('login', {
+      pageTitle,
+      errorMessage: 'Incorrect password',
+    });
+  }
+  return res.redirect('/');
+};
 export const logout = (req, res) => res.send('Logout');
 export const see = (req, res) => res.send('See Profile');
