0d77f6f4f379157058e86b99adfcc2f98d860704
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-24 11:28:43 +0900

    feat: create User model schema

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+import mongoose, { model } from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  location: String,
+});
+
+export const User = mongoose.model('User', userSchema);
