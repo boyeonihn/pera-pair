7511bda5c92c898fdfce45614d1a5364b355daf9
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-24 13:46:58 +0900

    build: install and implement sessions

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 // const express = require('express'); <- instead of this
 
 import express from 'express';
+import session from 'express-session';
 import morgan from 'morgan';
 import { rootRouter } from './routers/rootRouter';
 import { videoRouter } from './routers/videoRouter';
@@ -14,6 +15,13 @@ app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'Hello!',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use('/', rootRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
