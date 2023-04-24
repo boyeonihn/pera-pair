233a9b6a519be77bf58002202a4b5b79fd0aca23
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-24 11:29:48 +0900

    feat: create join page get and post method

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,21 @@
-export const join = (req, res) => res.send('JOIN!');
+import { User } from '../models/User';
+export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
+
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, location } = req.body;
+
+  await User.create({
+    email,
+    username,
+    password,
+    location,
+    name,
+  });
+  return res.redirect('/login');
+};
+
 export const edit = (req, res) => res.send('Editing Profile');
 export const remove = (req, res) => res.send('Delete User');
-export const login = (req, res) => res.send('Login');
+export const login = (req, res) => res.render('login', { pageTitle: 'Login' });
 export const logout = (req, res) => res.send('Logout');
 export const see = (req, res) => res.send('See Profile');
