68e3ee98e1173bebda9f2f61c4c35191db8803ef
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-26 10:50:10 +0900

    feat: add github OAuth login router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { remove, edit, see, logout } from '../controllers/userController';
+import {
+  remove,
+  edit,
+  see,
+  logout,
+  startGithubLogin,
+  finishGithubLogin,
+} from '../controllers/userController';
 
 export const userRouter = express.Router();
 
 userRouter.get('/edit', edit);
 userRouter.get('/delete', remove);
 userRouter.get(':id', see);
+userRouter.get('/github/start', startGithubLogin);
+userRouter.get('/github/finish', finishGithubLogin);
