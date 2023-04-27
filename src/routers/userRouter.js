e429cb035c714b2720db9933697e9ba06e536e69
Boyeon Ihn <boyeonihn@gmail.com>
2023-04-27 11:16:05 +0900

    feat: implement loggedin status specific middleware

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,15 +4,15 @@ import {
   getEdit,
   postEdit,
   see,
-  logout,
   startGithubLogin,
   finishGithubLogin,
 } from '../controllers/userController';
+import { protectUrlMiddleware, publicOnlyMiddleware } from '../middlewares';
 
 export const userRouter = express.Router();
 
-userRouter.route('/edit').get(getEdit).post(postEdit);
+userRouter.route('/edit').all(protectUrlMiddleware).get(getEdit).post(postEdit);
 userRouter.get('/delete', remove);
 userRouter.get(':id', see);
-userRouter.get('/github/start', startGithubLogin);
-userRouter.get('/github/finish', finishGithubLogin);
+userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin);
+userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin);
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,14 +7,23 @@ import {
   postUpload,
   deleteVideo,
 } from '../controllers/videoController';
+import { protectUrlMiddleware } from '../middlewares';
 
 export const videoRouter = express.Router();
 
-videoRouter.route('/upload').get(getUpload).post(postUpload); // need to put this on the top because express might consider "upload" as a valid :id parameter
+videoRouter
+  .route('/upload')
+  .all(protectUrlMiddleware)
+  .get(getUpload)
+  .post(postUpload); // need to put this on the top because express might consider "upload" as a valid :id parameter
 videoRouter.get('/:id([0-9a-f]{24})', watch);
 // videoRouter.get('/:id/edit', getEdit);
 // videoRouter.post('/:id/edit', postEdit);
 // You can do both post/get request in one line of code
 
-videoRouter.route('/:id([0-9a-f]{24})/edit').get(getEdit).post(postEdit);
-videoRouter.get('/:id/delete', deleteVideo);
+videoRouter
+  .route('/:id([0-9a-f]{24})/edit')
+  .all(protectUrlMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter.all(protectUrlMiddleware).get('/:id/delete', deleteVideo);
