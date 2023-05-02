6c5f9ed00fcece0b06cc6bed7dbff1cf0651d372
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-02 19:06:42 +0900

    feat: add play and mute controls

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,33 @@
+const video = document.querySelector('video');
+const playBtn = document.getElementById('play');
+const muteBtn = document.getElementById('mute');
+const currentTime = document.getElementById('currentTime');
+const totalTime = document.getElementById('totalTime');
+const volumeRange = document.getElementById('volume');
+const timeline = document.getElementById('timeline');
+const fullscreenBtn = document.getElementById('fullscreen');
+const videoContainer = document.getElementById('videoContainer');
+const videoControls = document.getElementById('videoControls');
+const handlePlay = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+
+  play.innerText = video.paused ? 'Pause' : 'Play';
+};
+
+const handleMute = (event) => {
+  if (!video.muted) {
+    video.muted = true;
+  } else {
+    video.muted = false;
+  }
+
+  muteBtn.innerText = video.muted ? 'Unmute' : 'Mute';
+  volumeRange.value = video.muted ? 0 : userVolume;
+};
+
+playBtn.addEventListener('click', handlePlay);
+muteBtn.addEventListener('click', handleMute);
