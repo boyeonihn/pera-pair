7dd4eb64324ebf59e4a3e1aa2eca2aaa69a22841
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-05 10:09:43 +0900

    feat: add handlers for start, stop, download video recording

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -8,6 +8,41 @@ let stream;
 let recorder;
 let videoFile;
 
+const startRecording = () => {
+  recordBtn.innerText = stopText;
+  recorder = new window.MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data); // URL created by the browser - to access a file (on the memory of the browser)
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const stopRecording = () => {
+  recordBtn.innerText = downloadText;
+  recorder.stop();
+};
+
+const handleDownload = () => {
+  const videoLink = document.createElement('a');
+  videoLink.href = videoFile;
+  videoLink.download = 'MyRecording.webm';
+  document.body.appendChild(videoLink);
+  videoLink.click();
+};
+const handleRecording = () => {
+  if (recordBtn.innerText === startText) {
+    startRecording();
+  } else if (recordBtn.innerText === stopText) {
+    stopRecording();
+  } else if (recordBtn.innerText === downloadText) {
+    handleDownload();
+  }
+};
+
 const init = async () => {
   try {
     stream = await navigator.mediaDevices.getUserMedia({
@@ -22,3 +57,4 @@ const init = async () => {
 };
 
 init();
+recordBtn.addEventListener('click', handleRecording);
