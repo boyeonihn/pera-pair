81ac3b03c9a2150ebc4afe4a34f0710aeb811906
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-06 19:04:44 +0900

    feat: add ffmpeg function to convert webm to mp4 recording

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,4 @@
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 const recordBtn = document.getElementById('recordBtn');
 const video = document.getElementById('preview');
 const startText = 'Start Recording';
@@ -26,10 +27,23 @@ const stopRecording = () => {
   recorder.stop();
 };
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load(); // ffmpeg - user is going to run software that has non-JS code
+  // (it's heavy - need to wait) - user computer is the one who handles the burden
+
+  // create file from recording.webm
+  ffmpeg.FS('writeFile', 'recording.webm', await fetchFile(videoFile));
+
+  await ffmpeg.run('-i', 'recording.webm', '-r', '60', 'output.mp4'); // usually run this on console/terminal but because we loaded on browser,
+  // we can use same commands
+  const mp4File = ffmpeg.FS('readFile', 'output.mp4');
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: 'video/mp4' });
+  const mp4Url = URL.createObjectURL(mp4Blob);
   const videoLink = document.createElement('a');
-  videoLink.href = videoFile;
-  videoLink.download = 'MyRecording.webm';
+  videoLink.href = mp4Url; // binary data
+  videoLink.download = 'MyRecording.mp4';
   document.body.appendChild(videoLink);
   videoLink.click();
 };
