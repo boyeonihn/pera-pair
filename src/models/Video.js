6f50d150fb5014303c8f32ea85c1c917156800c9
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-09 16:42:05 +0900

    feat: add comments data to User and Video schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -13,6 +13,9 @@ const videoSchema = new mongoose.Schema({
     rating: { type: Number, default: 0, required: true },
   },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  comments: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Comment' },
+  ],
 });
 
 //middleware must go before the model is made
