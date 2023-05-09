699402461f41738f6d2dc4c6c1a7119ee9720913
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-09 11:15:27 +0900

    refactor: refactor code by replacing string with object properties for error prevention

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,24 @@
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 const recordBtn = document.getElementById('recordBtn');
 const video = document.getElementById('preview');
-const startText = 'Start Recording';
-const stopText = 'Stop Recording';
-const downloadText = 'Download Recording';
+
+const recordStatus = {
+  startText: 'Start Recording',
+  stopText: 'Stop Recording',
+  downloadText: 'Download Recording',
+  transcodeText: 'Transcoding Video',
+  recordAgain: 'Record Again',
+};
 
 let stream;
 let recorder;
 let videoFile;
+
+const files = {
+  input: 'recording.webm',
+  output: 'output.mp4',
+  thumb: 'thumbnail.jpg',
+};
 
 const downloadFile = (fileUrl, fileName) => {
   const link = document.createElement('a');
@@ -18,7 +29,7 @@ const downloadFile = (fileUrl, fileName) => {
 };
 
 const startRecording = () => {
-  recordBtn.innerText = stopText;
+  recordBtn.innerText = recordStatus.stopText;
   recorder = new window.MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data); // URL created by the browser - to access a file (on the memory of the browser)
@@ -31,33 +42,34 @@ const startRecording = () => {
 };
 
 const stopRecording = () => {
-  recordBtn.innerText = downloadText;
+  recordBtn.innerText = recordStatus.downloadText;
   recorder.stop();
 };
 
 const handleDownload = async () => {
+  recordBtn.innerText = recordStatus.transcodeText;
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load(); // ffmpeg - user is going to run software that has non-JS code
   // (it's heavy - need to wait) - user computer is the one who handles the burden
 
   // create file from recording.webm
-  ffmpeg.FS('writeFile', 'recording.webm', await fetchFile(videoFile));
+  ffmpeg.FS('writeFile', files.input, await fetchFile(videoFile));
 
-  await ffmpeg.run('-i', 'recording.webm', '-r', '60', 'output.mp4'); // usually run this on console/terminal but because we loaded on browser,
+  await ffmpeg.run('-i', files.input, '-r', '60', files.output); // usually run this on console/terminal but because we loaded on browser,
   // we can use same commands
 
   await ffmpeg.run(
     '-i',
-    'recording.webm',
+    files.input,
     '-ss',
     '00:00:01',
     '-frames:v',
     '1',
-    'thumbnail.jpg'
+    files.thumb
   );
 
-  const mp4File = ffmpeg.FS('readFile', 'output.mp4');
-  const thumbFile = ffmpeg.FS('readFile', 'thumbnail.jpg');
+  const mp4File = ffmpeg.FS('readFile', files.output);
+  const thumbFile = ffmpeg.FS('readFile', files.thumb);
 
   const mp4Blob = new Blob([mp4File.buffer], { type: 'video/mp4' });
   const thumbBlob = new Blob([thumbFile.buffer], { type: 'image/jpg' });
@@ -69,20 +81,21 @@ const handleDownload = async () => {
   downloadFile(thumbUrl, 'MyThumbnail.jpg');
 
   // deleting files - after they are downloaded
-  ffmpeg.FS('unlink', 'recording.webm');
-  ffmpeg.FS('unlink', 'output.mp4');
-  ffmpeg.FS('unlink', 'thumbnail.jpg');
+  ffmpeg.FS('unlink', files.input);
+  ffmpeg.FS('unlink', files.output);
+  ffmpeg.FS('unlink', files.thumb);
 
   URL.revokeObjectURL(thumbUrl);
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(videoFile);
+  recordBtn.innerText = recordStatus.recordAgain;
 };
 const handleRecording = () => {
-  if (recordBtn.innerText === startText) {
+  if (recordBtn.innerText === recordStatus.startText) {
     startRecording();
-  } else if (recordBtn.innerText === stopText) {
+  } else if (recordBtn.innerText === recordStatus.stopText) {
     stopRecording();
-  } else if (recordBtn.innerText === downloadText) {
+  } else if (recordBtn.innerText === recordStatus.downloadText) {
     handleDownload();
   }
 };
