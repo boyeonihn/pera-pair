9183c6bd228c181d3f3f4e667ec944f686ae51ef
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-11 11:53:15 +0900

    feat: add deleteComment function

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -5,8 +5,24 @@ const videoContainer = document.getElementById('videoContainer');
 const videoComments = document.querySelectorAll('.video__comment');
 const videoId = videoContainer.dataset.id;
 
+const deleteComment = async (event) => {
+  const clickedEl = event.target;
+  if (clickedEl.tagName.toLowerCase() === 'i') {
+    const targetCommentEl = clickedEl.closest('li.video__comment');
+    const id = targetCommentEl.dataset.id;
 
-const addComment = (event) => {
+    await fetch(`/api/videos/${videoId}/comment`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+    });
+
+    targetCommentEl.remove();
+  }
+};
+
   event.preventDefault();
   if (form.dataset.login === 'false') {
     alert('Please Login First');
@@ -28,4 +44,6 @@ const addComment = (event) => {
   });
   textarea.value = '';
 };
-form.addEventListener('submit', addComment);
+videoComments.forEach((comment) =>
+  comment.addEventListener('click', deleteComment)
+);
