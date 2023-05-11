40ae0df9950b6c7275ed84315906355ce7049c5a
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-11 11:53:59 +0900

    feat: build function to render newly added comment to DOM

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -23,6 +23,23 @@ const deleteComment = async (event) => {
   }
 };
 
+const addComment = (commentInfo) => {
+  const videoComments = document.querySelector('.video__comments ul');
+  const comment = document.createElement('li');
+  comment.classList.add('video__comment');
+  comment.dataset.id = commentInfo.commentId;
+  comment.innerHTML = `
+  <span>
+  <a href="/users/${commentInfo.id}">${commentInfo.name}</a>
+  </span>
+  <span>${commentInfo.createdAt}</span>
+  <span><i class="fa-solid fa-delete-left"></i></span>
+  <p>${commentInfo.text}</p>
+  `;
+
+  comment.addEventListener('click', deleteComment);
+  videoComments.prepend(comment);
+};
   event.preventDefault();
   if (form.dataset.login === 'false') {
     alert('Please Login First');
