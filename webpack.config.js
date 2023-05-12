24c1bfa47872f7dce53ca82dbcba71423b6946f1
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-12 16:46:29 +0900

    build: update scripts and config for build/production

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,6 @@ module.exports = {
     recorder: BASE_JS + 'recorder.js',
     commentSection: BASE_JS + 'commentSection.js',
   },
-  mode: 'development',
   watch: true,
   plugins: [
     new MiniCssExtractPlugin({
