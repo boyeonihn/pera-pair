d1564d3414154a97319badba567959f6f93856f5
Boyeon Ihn <boyeonihn@gmail.com>
2023-05-29 20:55:01 +0900

    feat: build getPost controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,14 @@
-export const getPost = (req, res) => {
-  return res.render('posts/read', { pageTitle: 'Post' });
+import { User } from '../models/User';
+import { Post } from '../models/Post';
+
+export const getPost = async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findById(id).populate('owner');
+
+  if (!post) {
+    return res.render('404', { pageTitle: 'Post Not Found' });
+  }
+  return res.render('posts/read', { pageTitle: `Post: ${post.title}`, post });
 };
 export const getCreate = (req, res) => {
   return res.render('posts/create', { pageTitle: 'Create a post' });
