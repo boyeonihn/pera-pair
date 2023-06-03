7745a290dfe166f5dd5fb76ec3f3d35537b4a164
Boyeon Ihn <boyeonihn@gmail.com>
2023-06-03 19:11:36 +0900

    feat: rename videoComment controllers and add postComment controllers

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import {
-  registerView,
-  createComment,
-  deleteComment,
+  registerVideoView,
+  createVideoComment,
+  deleteVideoComment,
 } from '../controllers/videoController';
+import {
+  registerPostView,
+  createPostComment,
+  deletePostComment,
+} from '../controllers/postController';
 
 export const apiRouter = express.Router();
 
-apiRouter.post('/videos/:id([0-9a-f]{24})/view', registerView);
-apiRouter.post('/videos/:id([0-9a-f]{24})/comment', createComment);
-apiRouter.delete('/videos/:id([0-9a-f]{24})/comment', deleteComment);
+apiRouter.post('/videos/:id([0-9a-f]{24})/view', registerVideoView);
+apiRouter.post('/videos/:id([0-9a-f]{24})/comment', createVideoComment);
+apiRouter.delete('/videos/:id([0-9a-f]{24})/comment', deleteVideoComment);
+apiRouter.post('/posts/:id([0-9a-f]{24})/view', registerPostView);
+apiRouter.post('/posts/:id([0-9a-f]{24})/comment', createPostComment);
+apiRouter.delete('/posts/:id([0-9a-f]{24})/comment', deletePostComment);
